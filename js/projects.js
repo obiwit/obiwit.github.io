b6c29083439805dd2e5d06e92deb189d7e99150d
Beatriz Borges <beatrizmbribeiro@gmail.com>
2017-09-24 06:15:47 +0100

    Added (responsive) image previews for the modal image gallery; not sure if they'll stay though...

--- js/projects.js
+++ js/projects.js
@@ -65,6 +65,9 @@ $(function() {
 
      // add caption based on img's alt text
      $('#caption').html($image.attr('alt'));
+
+     // add navigational previews below the caption
+     addPreviews();
    }
 
    // Handles the click of a next of previous buttons/arrows
@@ -79,11 +82,42 @@ $(function() {
      if (next) {
        updateAmount = 0;
      }
-     console.log((imgIndex + updateAmount)%totalImgNum +1);
 
      $image = $("#" + imgProject).find("[data-index='" + (
                 (imgIndex + updateAmount)%totalImgNum +1) + "']");
                 // we add 1 to make it count from 1 to length (ie. versus going from 0 to length-1)
      modalLoadImage($image);
+   }
+
+   // Adds several smaller images below the principal, for navigational purposes
+   function addPreviews() {
+     // clear previous images
+     $('#previews').html('');
+
+     // add new images
+     var imgIndex = $('#main-image img').data()["index"];
+     var imgProject = $('#main-image img').data()["project"];
+     var totalImgNum = $("#" + imgProject + " div.accordion").data()["items"];
+
+     var screenWidth = $(window).width();
+
+     var indexes = [-3, -2, 0, 1];
+     var navInfo = ['Back two images', 'Previous image', 'Next image', 'Foward two images'];
+     if (screenWidth < 500) {
+       indexes = [-2, 0]
+       var navInfo = ['Previous image', 'Next image'];
+     } else if (screenWidth < 850) {
+       indexes = [-2, 0, 1];
+       var navInfo = ['Previous image', 'Next image', 'Foward two images'];
+     }
+
+     for(var i = 0; i < indexes.length; i++) {
+       var $img = $("#" + imgProject).find("[data-index='" + ((imgIndex+totalImgNum+indexes[i])%totalImgNum+1) + "']");
+       $('#previews').append($('<div class="preview"><span>'+navInfo[i]+'</span></div>').append($img.clone().css('margin', "0 " + parseInt($img.height()/$img.width() * 0.04 * screenWidth) + "px" )));
+     }
+
+     $('#previews div img').click(function(event) {
+       modalLoadImage($(event.target).css('margin', "0"));
+     });
    }
 });
