4177174489a2d50187469d066a6fc1a6a1add425
Beatriz Borges <beatrizmbribeiro@gmail.com>
2018-03-19 09:02:22 +0000

    Kind of horizontal scroller add to (top) projects page

--- js/projects.js
+++ js/projects.js
@@ -1,204 +1,79 @@
 $(function() {
-  /**
-   * Accordion Logic
-   */
-  $('button.toggle-accordion').on('click', function() {
-    var elem = $(this);
-    toggleAccordion(elem);
-  });
-  function toggleAccordion(accordion) {
-    var nextElemText = accordion.hasClass('expanded') ? 'See more' : 'See less';
-    accordion.toggleClass('expanded');
-    accordion.html(nextElemText);
-
-    var accordionContent = accordion.next();
-    accordionContent.toggle();
-  }
-
 
   /**
    * Image Gallery Pop-Up/Modal Logic
    */
 
    // Event Listeners
-   $('#projects div.accordion img').on('click', function(event) {
-     // Open a modal window (called when an image is clicked)
-     $('#modal-gallery').toggle();
-     modalLoadImage($(event.target));
-   });
-   $('#modal-gallery span.close').on('click', closeModal);
-   $('#modal-gallery a.next').on('click', function(event) {
+   $('div#top-projects a.next').on('click', function(event) {
      handleNextPrevious(true);
    });
-   $('#modal-gallery a.prev').on('click', function(event) {
+   $('div#top-projects a.prev').on('click', function(event) {
      handleNextPrevious(false)
    });
-   $(window).on('hashchange', function(){
-     var hash = window.location.hash;
-     if (!window.location.hash) {
-       closeModal();
-     } else {
-       url_parts = hash.split("/");
-       if (url_parts.length != 3 || url_parts[2] != $('#modal-gallery img').data('index')) {
-         window.location.reload(true);
-       }
-     }
-   });
-   // handle (phone and tablet's) swiping events as well
-   var modal_el = document.getElementById('modal-gallery');
-   var mc = new Hammer(modal_el);
-   mc.on("swiperight", function(ev) {
-       handleNextPrevious(true);
-   });
-   mc.on("swipeleft", function(ev) {
-       handleNextPrevious(false);
-   });
-
-   // check if landing url is a "modal" url, and if so, open corresponding modal
-   var hash = window.location.hash; //window.location.href.split('#')[1];
-   if (hash) {
-     var url_parts = hash.split("/");
-     if (url_parts.length == 3) {
-       var list_elem_id = "li#"+ url_parts[1];
-       // open the modal immediately, so the website appears faster
-       $('#modal-gallery').toggle();
-
-       // open corresponding accordion
-       toggleAccordion($(list_elem_id + " button.toggle-accordion"));
-       // scroll to it
-       $('html, body').animate({
-         scrollTop: $(list_elem_id).offset().top
-       }, 1000);
-
-       // add image to the modal
-       var $image = $(list_elem_id + " div.accordion img[data-index='"+ url_parts[2] +"']");
-       setTimeout(function(){ modalLoadImage($image); }, 1000); // 500 is too little for some images
-     }
-   }
-
-   // Close a modal window
-   function closeModal() {
-     history.replaceState(null, null, '/projects.html');
-     $('#modal-gallery').hide();
-     $('#main-image').html('');
-   }
-
-   // load an image into the main content part of the modal window (doesn't open
-   // the modal window itself)
-   function modalLoadImage($image) {
-     // guarantee modal is clean
-     $('#main-image').html('');
-     $('#modal-gallery a.prev').show();
-     $('#modal-gallery a.next').show();
-
-     // update url
-     window.location.hash = 'modal/' + $image.data('project') + '/' + $image.data('index');
-
-     // add selected image to the modal, and show it
-     $('#main-image').append($image.clone());
-
-     var imageHeight = $image.height();
-     var imageWidth = $image.width();
-     if (imageHeight > imageWidth) {
-       $('#main-image').css('maxWidth', (imageWidth/imageHeight * 65) + "vh" );
-     } else {
-       $('#main-image').css('maxWidth', (imageWidth/imageHeight * 60) + "vh" );
-     }
-     var gallerySize = $image.parent().data()["items"];
-     if(gallerySize <= 1) {
-       $('#main-image').css('maxWidth', (imageWidth/imageHeight * 80) + "vh" );
-       if (gallerySize == 1) {
-         // if the gallery only has one element, hide the next and previous buttons
-         $('#modal-gallery a.prev').hide();
-         $('#modal-gallery a.next').hide();
-       }
-     }
-     // add caption based on img's alt text
-     var caption = $image.attr('alt');
-
-     // add pre-caption (bold italized text before the actual caption)if it exists
-     if ($image.data()["pre"]){
-       caption = '<strong><em>' + $image.data()["pre"] + '</em></strong>' + caption;
-     }
-
-     // add caption itself, with a very slight delay that creates a more 'fluid' experience
-     $('#caption').html(caption);
-     $('#caption').hide();
-     setTimeout(function() { $('#caption').show(); }, 100);
-
-     // add PS (sub-caption) if it exists
-     if ($image.data()["ps"]){
-        $('#caption').append('<br><span class="small" style="color: #bbb">'+$image.data()["ps"]+'</span>');
-     }
-
-     // add navigational previews below the caption
-     addPreviews();
-   }
+   // $(window).on('hashchange', function(){
+   //   var hash = window.location.hash;
+   //   if (!window.location.hash) {
+   //     closeModal();
+   //   } else {
+   //     url_parts = hash.split("/");
+   //     if (url_parts.length != 3 || url_parts[2] != $('#modal-gallery img').data('index')) {
+   //       window.location.reload(true);
+   //     }
+   //   }
+   // });
+   // // handle (phone and tablet's) swiping events as well
+   // var modal_el = document.getElementById('modal-gallery');
+   // var mc = new Hammer(modal_el);
+   // mc.on("swiperight", function(ev) {
+   //     handleNextPrevious(true);
+   // });
+   // mc.on("swipeleft", function(ev) {
+   //     handleNextPrevious(false);
+   // });
+   //
+   // // check if landing url is a "modal" url, and if so, open corresponding modal
+   // var hash = window.location.hash; //window.location.href.split('#')[1];
+   // if (hash) {
+   //   var url_parts = hash.split("/");
+   //   if (url_parts.length == 3) {
+   //     var list_elem_id = "li#"+ url_parts[1];
+   //     // open the modal immediately, so the website appears faster
+   //     $('#modal-gallery').toggle();
+   //
+   //     // open corresponding accordion
+   //     toggleAccordion($(list_elem_id + " button.toggle-accordion"));
+   //     // scroll to it
+   //     $('html, body').animate({
+   //       scrollTop: $(list_elem_id).offset().top
+   //     }, 1000);
+   //
+   //     // add image to the modal
+   //     var $image = $(list_elem_id + " div.accordion img[data-index='"+ url_parts[2] +"']");
+   //     setTimeout(function(){ modalLoadImage($image); }, 1000); // 500 is too little for some images
+   //   }
+   // }
 
    // Handles the click of a next of previous buttons/arrows
    // The parameter is a boolean: true for next image; false for previous image
    function handleNextPrevious(next) {
-     var $image;
-     var imgIndex = $('#main-image img').data()["index"];
-     var imgProject = $('#main-image img').data()["project"];
-     var totalImgNum = $("#" + imgProject + " div.accordion").data()["items"];
+     var $newProj;
+     var totalProjects = 4; // 4 projects total
+     var projectIndex = $('div#top-projects div.project').not(".hidden").data()["index"]
+                            + totalProjects; // to garantee there are never negative numbers
 
-     var updateAmount = totalImgNum-2; // previous image by default
+     var updateAmount = -2; // previous image by default
      if (next) {
        updateAmount = 0;
      }
+     console.log("[data-index='" + (
+                (projectIndex + updateAmount)%totalProjects + 1) + "']");
 
-     $image = $("#" + imgProject).find("[data-index='" + (
-                (imgIndex + updateAmount)%totalImgNum +1) + "']");
-                // we add 1 to make it count from 1 to length (ie. versus going from 0 to length-1)
-     modalLoadImage($image);
+     $('div#top-projects div.project').not(".hidden").addClass("hidden");
+     $('*[data-index="' + ((projectIndex + updateAmount)%totalProjects + 1)
+         + '"]').removeClass("hidden");
+      // $('div#top-projects div.project').find("[data-index='" + (
+      //            (projectIndex + updateAmount)%totalProjects +1) + "']");
    }
 
-   // Adds several smaller images below the principal, for navigational purposes
-   // Doesn't show image previews if there aren't enough images in a gallery (ie. at least 3)
-   function addPreviews() {
-     // clear previous images
-     $('#previews').html('');
-     $('#previews').hide();
-     setTimeout(function() {
-       $('#previews').show();
-       $('#previews span').hide();
-       setTimeout(function() { $('#previews span').show(); }, 100);
-     }, 100);
-     // the above lines create a more 'fluid' experience
-
-     // add new images
-     var imgIndex = $('#main-image img').data()["index"];
-     var imgProject = $('#main-image img').data()["project"];
-     var totalImgNum = $("#" + imgProject + " div.accordion").data()["items"];
-
-     var screenWidth = $(window).width();
-
-     var indexes = [-3, -2, 0, 1];
-     var navInfo = ['Back two images', 'Previous image', 'Next image', 'Foward two images'];
-     if (totalImgNum <= 1) {
-       indexes = [];
-     } else if (totalImgNum <= 2) {
-       indexes = [0];
-       var navInfo = ['Next image'];
-     } else if (screenWidth < 500 || totalImgNum <= 3) {
-       indexes = [-2, 0]
-       var navInfo = ['Previous image', 'Next image'];
-     } else if (screenWidth < 850 || totalImgNum <= 4) {
-       indexes = [-2, 0, 1];
-       var navInfo = ['Previous image', 'Next image', 'Foward two images'];
-     }
-
-     for(var i = 0; i < indexes.length; i++) {
-       var $image = $("#" + imgProject).find("[data-index='" + ((imgIndex+totalImgNum+indexes[i])%totalImgNum+1) + "']");
-       $('#previews').append($('<div class="preview"><span>'+navInfo[i]+'</span></div>').append($image.clone().css('margin', "0 " + parseInt($image.height()/$image.width() * 0.04 * screenWidth) + "px" )));
-     }
-
-     $('#previews span').on('click', function(event) {
-       modalLoadImage($(event.target).next().css('margin', "0"));
-     });
-     $('#previews img').on('click', function(event) {
-       modalLoadImage($(event.target).css('margin', "0"));
-     });
-   }
 });
