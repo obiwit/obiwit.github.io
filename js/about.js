dbcefe11bea540012511e4eae2eabfadad5f63cb
Beatriz Borges <beatrizmbribeiro@gmail.com>
2018-08-09 20:11:34 +0100

    Created modal pdf gallery for specializations

--- js/about.js
+++ js/about.js
@@ -4,29 +4,76 @@ $(function() {
   var modal_url = window.location.href.split('#')[1];
   if (modal_url) {
     var url_parts = modal_url.split("/");
-    if (url_parts.length == 3) {
+
+    if (url_parts.length == 3) { // simple modal window
       $('#modal-gallery').toggle();
       if (url_parts[1] == "vid") { // open video modal_url
+        // url_parts[2] = video id
         loadModalVideo(url_parts[2]);
       } else if (url_parts[1] == "pdf") { // open PDF modal
-        loadModalPDF(url_parts[1] + "/" +url_parts[2]);
+        // url_parts[2] = pdf path
+        loadModalPDF(url_parts[1] + "/" + url_parts[2]);
       }
+    } else if (url_parts[1] == "pdf-gallery") { // open a PDF gallery modal
+      // url_parts[2] = group id
+      // url_parts[3] = current index
+      $('#modal-gallery').toggle();
+      loadModalPDF("pdf/" + url_parts[2] + url_parts[3] + ".pdf");
+
+      // add next and prev arrows
+      $('#modal-gallery').append('<a class="prev cursor">&#10094;</a><a class="next cursor">&#10095;</a>');
+
+      $('#modal-gallery a.next').on('click', function(event) {
+        handleNextPrevious(true);
+      });
+      $('#modal-gallery a.prev').on('click', function(event) {
+        handleNextPrevious(false);
+      });
+    }
+  }
+  function handleNextPrevious(next) {
+    var modal_url = window.location.href.split('#')[1];
+    if (modal_url) {
+      var url_parts = modal_url.split("/");
+
+      // get gallery size and updateAmount
+      var $gallery = $('#home').find("[data-gallery='" + url_parts[2] + "']");
+      var gallerySize = $gallery.data()["items"];
+      var updateAmount = next? 0 : gallerySize-2;
+
+      // get new index, update url and load next/prev pdf
+      var newIndex = (Number(url_parts[3]) + updateAmount) % gallerySize + 1;
+      window.location.hash = 'modal/' + url_parts[1] + '/' + url_parts[2] + '/' + newIndex;
+      loadModalPDF("pdf/" + url_parts[2] + newIndex + ".pdf");
     }
   }
 
   // Event Listeners
   /**************************************** GENERAL PURPOSE */
-  // close modal on 'escape' key press
+  // close modal on 'escape' key press, handle side arrows to navigate modal, if gallery
   document.onkeydown = function(evt) {
     evt = evt || window.event;
-    var isEscape = false;
+    var isEscape = false, isRightArrow = false, isLeftArrow = false;
     if ("key" in evt) {
         isEscape = (evt.key == "Escape" || evt.key == "Esc");
+        isRightArrow = (evt.key == "ArrowRight");
+        isLeftArrow = (evt.key == "ArrowLeft");
     } else {
         isEscape = (evt.keyCode == 27);
+        isRightArrow = (evt.keyCode == 39);
+        isLeftArrow = (evt.keyCode == 37);
     }
     if (isEscape) {
         closeModal();
+    }
+    // check if modal is a modal gallery
+    else if (window.location.href.split('#')[1].split("/")[1] == "pdf-gallery") {
+
+       if (isRightArrow) {
+          handleNextPrevious(true);
+      } else if (isLeftArrow) {
+          handleNextPrevious(false);
+      }
     }
   };
 
@@ -56,22 +103,43 @@ $(function() {
    }
   });
 
-  // Open a modal window (called when an image is clicked)
+  // Open a modal window (called when a link is clicked)
   function openModal($a) {
 
     var href = $a.attr('href');
     $('#modal-gallery').toggle();
 
+    // video modal window
     if ($a.hasClass("video")) {
       var video_id = href.split('=')[1];
       window.location.hash = 'modal/vid/' + video_id;
 
       loadModalVideo(video_id);
     }
+
+    // pdf modal window
     else if ($a.hasClass("pdf")) {
       window.location.hash = 'modal' + href;
 
       loadModalPDF(href);
+    }
+
+    // pdf window modal window
+    else if ($a.hasClass("pdf-gallery")) {
+      window.location.hash = 'modal' + href;
+
+      url_parts = href.split("/");
+      loadModalPDF("pdf/" + url_parts[2] + url_parts[3] + ".pdf");
+
+      // add next and prev arrows
+      $('#modal-gallery').append('<a class="prev cursor">&#10094;</a><a class="next cursor">&#10095;</a>');
+
+      $('#modal-gallery a.next').on('click', function(event) {
+        handleNextPrevious(true);
+      });
+      $('#modal-gallery a.prev').on('click', function(event) {
+        handleNextPrevious(false);
+      });
     }
   }
 
@@ -98,6 +166,10 @@ $(function() {
     history.replaceState(null, null, '/index.html');
     $('#modal-gallery').hide();
     $('#viewer').html('');
+
+    // remove next and prev buttons, if they existed
+    $('#modal-gallery a.next').remove();
+    $('#modal-gallery a.prev').remove();
   }
 
 
